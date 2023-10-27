dbf03df5c66926e74c4b5e193098a8be7ca2e7ad
César Berry <cesarberry63@gmail.com>
2023-10-27 12:12:52 +0200

    [ Refactoring Final ]

--- src/main/java/theatricalplays/StatementPrinter.java
+++ src/main/java/theatricalplays/StatementPrinter.java
@@ -2,21 +2,18 @@ package theatricalplays;
 import java.text.NumberFormat;
 import java.util.Locale;
 
-
 public class StatementPrinter {
 
   public String print(Invoice invoice) 
   {
-
-    String name ;
     StringBuffer result     = new StringBuffer("Statement for " + invoice.customer + "\n");
     NumberFormat frmt       = NumberFormat.getCurrencyInstance(Locale.US);
     
     for (Performance perf : invoice.performances) // La boucle for nous permet de parcourir toutes les performance presentes dans notre liste
                                                   // de performances dans notre classe invoice.
     {
-      name = perf.play.name;                      // Pour chaque performance, on va calculer le prix unitaire et l'afficher a coté du nom de la piece
-      result.append(String.format("  %s: %s (%s seats)\n", name , frmt.format(perf.play.getPrice(perf.audience)), perf.audience));
+      // Pour chaque performance, on va calculer le prix unitaire et l'afficher a coté du nom de la piece
+      result.append(String.format("  %s: %s (%s seats)\n", perf.play.name , frmt.format(perf.play.getPrice(perf.audience)), perf.audience));
     }
 
     result.append(String.format("Amount owed is %s\n", frmt.format(invoice.SommePrix())));  // On affiche ensuite le montant total de la facture
